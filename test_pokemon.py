d20786ec7d7d09423eff82b22f444e90269d9976
Victor <victor.santos.cd@gmail.com>
2022-09-04 15:14:44 -0300

    fix: Fix some errors in test

--- test_pokemon.py
+++ test_pokemon.py
@@ -6,29 +6,16 @@ class TestPokemonApi(unittest.TestCase):
     def setUp(self):
         self.pokemon_name = "Pikachu"
 
-    @patch("pokemon_top.Pokemon.realize_get")
-    def test_format_return_types(self, mocked_pokemon_list):
+    def test_format_return_types(self):
         """Must return a list instance."""
-        mocked_pokemon_list = MagicMock()
-        mocked_pokemon_list.realize_get.return_value = [[
-	{
-		"number": "25",
-		"name": "Pikachu",
-		"species": "Mouse",
-		"types": [
-			"Electric"
-	],}]
-    ]
         pokemon_obj = Pokemon(pokemon_name="Pikachu")
         self.assertIsInstance(pokemon_obj.return_types(), list)
 
 
-    @patch("pokemon_top.Pokemon.realize_get")
-    @patch("pokemon_top.Pokemon.return_types")
+    @patch("pokemon_top.Pokemon.realize_get", return_value={"name": "Pikachu", "types": ["Eletric"]})
+    @patch("pokemon_top.Pokemon.return_types", return_value=["Eletric"])
     def test_qnty_format(self, mocked_pokemon_list: MagicMock, mocked_pokemon_get: MagicMock):
         """Must return a int instance."""
-        mocked_pokemon_get.return_values = {"name": "Pikachu", "types": ["Eletric"]}
-        mocked_pokemon_list.return_values = ["Eletric"]
         pokemon_obj = Pokemon(pokemon_name="Pikachu")
         self.assertIsInstance(pokemon_obj.number_of_types(), int)
 
